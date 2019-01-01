ddc3d27da3304d361c4237c3d3596e2345a75be2
MiniLau <laurent@desausoi.net>
2019-01-01 21:59:06 +0100

    Server side advanced

--- home/models.py
+++ home/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class IP(models.Model):
-    ip = models.CharField(max_length=50)
-
-class User(models.Model):
+class People(models.Model):
     name = models.CharField(max_length=50)
     picture = models.ImageField(upload_to='pictures')
-    ips = models.ForeignKey(IP, on_delete=models.CASCADE)
+
+class IP(models.Model):
+    people = models.ForeignKey(User, on_delete=models.CASCADE)
+    ip = models.CharField(max_length=50)
--- home/views.py
+++ home/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
-import subprocess
+from home.models import People, IP
+import os
 
-SCRIPT_PATH = "./scan_network.sh"
+def home(request):
+    return render(request, 'home/home.html', scan())
 
-def scan(ip):
-   return subprocess.call([SCRIPT_PATH, ip])
+def scan():
+    states = []
+    for people in People.objects.all().order_by('-name'):
+        states.append( {'name' : people.name,
+                        'picture': people.picture,
+                        'is_online': is_people_online(people)} )
+    return states
+
+def is_people_online(people):
+    for ip in IP.objects.filter(people=people):
+        if os.system("ping -o -c 3 -W 3000 {}".format(ip)) != 0:
+            return True
+    return False
