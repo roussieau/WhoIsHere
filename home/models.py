e801bfc9372139639d824697dd9603bd078d2036
MiniLau <laurent@desausoi.net>
2019-01-02 19:23:29 +0100

    Dynamic layout

--- home/models.py
+++ home/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+
 class People(models.Model):
     name = models.CharField(max_length=50)
-    picture = models.ImageField(upload_to='pictures')
+    photo = models.ImageField(upload_to='home/photos/')
+
+    def __str__(self):
+        return self.name
+
 
 class IP(models.Model):
-    people = models.ForeignKey(User, on_delete=models.CASCADE)
+    people = models.ForeignKey(People, on_delete=models.CASCADE, default=None)
     ip = models.CharField(max_length=50)
+
+    class Meta:
+        verbose_name = 'IP'
+        verbose_name_plural = 'IPs'
+
+    def __str__(self):
+        return self.ip
--- home/views.py
+++ home/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from home.models import People, IP
+from .models import People, IP
 import os
 
-def home(request):
-    return render(request, 'home/home.html', scan())
+def index(request):
+    return render(request, 'home/index.html', {'peoples_status': get_status()})
 
-def scan():
-    states = []
-    for people in People.objects.all().order_by('-name'):
-        states.append( {'name' : people.name,
-                        'picture': people.picture,
-                        'is_online': is_people_online(people)} )
-    return states
+
+def get_status():
+    """ Return the status of the every People in the database """
+    return [{'name': people.name,
+            'photo': people.photo,
+            'is_online': is_people_online(people)}
+        for people in People.objects.all().order_by('-name')]
 
 def is_people_online(people):
+    """ Check if the given People is connected to the network """
     for ip in IP.objects.filter(people=people):
-        if os.system("ping -o -c 3 -W 3000 {}".format(ip)) != 0:
+        if os.system("ping -o -c 3 -W 3000 {}".format(ip.ip)) == 0:
             return True
     return False
